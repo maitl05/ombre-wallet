e6dca4bf7c5d6c5447113e78ce46f7feeea00cd6
maitl <mahdiaminigm2@gmail.com>
2022-09-15 20:29:53 +0430

    fix active navLink color

--- renderer/components/NavLink/index.tsx
+++ renderer/components/NavLink/index.tsx
@@ -27,9 +27,9 @@ export default function NavLink({
       <Link href={href} passHref>
         <a
           className={cn(
-            'flex text-lg gap-3 items-center border-b-2 border-primary-600 px-3 py-2',
-            isActive && 'font-extrabold',
-            'hover:text-white transition-colors',
+            'flex text-lg gap-3 items-center border-b-2 border-primary-600 px-3 py-2 transition-colors',
+            isActive && 'font-extrabold text-secondary-300',
+            !isActive && 'hover:text-white',
           )}>
           {icon}
           {title}
