6db6bb8c8eba826af3e681c95e8ba4a62483fde4
maitl <mahdiaminigm2@gmail.com>
2022-09-15 20:29:08 +0430

    add broken address validator

--- renderer/helpers/validators.ts
+++ renderer/helpers/validators.ts
@@ -7,3 +7,64 @@ export const testHelloValidator: ValidatorFunction = (value) => {
     return undefined
   }
 }
+
+// TODO WARNING this code is incorrect fix later
+export const paymentIdValidator: ValidatorFunction = (value) => {
+  if (value.length !== 0) {
+    if (
+      /^[0-9A-Fa-f]+$/.test(value) &&
+      (value.length == 16 || value.length == 64)
+    ) {
+      return undefined
+    } else {
+      return 'Invalid Payment ID'
+    }
+  } else {
+    return undefined
+  }
+}
+
+// TODO WARNING this code is incorrect fix later
+export const privkeyValidator: ValidatorFunction = (value) => {
+  if (value.length !== 0) {
+    if (/^[0-9A-Fa-f]+$/.test(value) && value.length == 64) {
+      return undefined
+    } else {
+      return 'Invalid Private Key'
+    }
+  } else {
+    return undefined
+  }
+}
+
+// TODO WARNING this code is incorrect fix later
+export const addressValidator: ValidatorFunction = (value) => {
+  if (!/^[0-9A-Za-z]+$/.test(value)) return 'Address must be base58'
+
+  switch (value.substring(0, 4)) {
+    case 'Shade':
+    case 'cash':
+    case 'Suto':
+    case 'Te':
+      return value.length === 99 ? undefined : 'Invalid Address'
+
+      return value.length == 98 ? undefined : 'Invalid Address'
+
+    case 'Suba':
+    case 'Ts':
+      return value.length == 99 ? undefined : 'Invalid Address'
+
+    case 'Shadow':
+    case 'casi':
+    case 'Suti':
+    case 'Ti':
+      return value.length === 110 ? undefined : 'Invalid Address'
+
+    case 'Shad3':
+    case 'Tu':
+      return value.length === 55 ? undefined : 'Invalid Address'
+
+    default:
+      return 'Invalid Address'
+  }
+}
