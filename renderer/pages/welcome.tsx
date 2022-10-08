f466b8d005be7e86e71c2aab2876e5abbfadcee1
maitl <mahdiaminigm2@gmail.com>
2022-10-08 16:48:22 +0200

    add welcome page

---
+++ renderer/pages/welcome.tsx
@@ -0,0 +1,31 @@
+import React, { useRef } from 'react'
+import { useRouter } from 'next/router'
+import { useMatrixEffect } from 'hooks/matrix-effect'
+import Button from 'components/Button'
+
+function WelcomePage() {
+  const canvas = useRef<HTMLCanvasElement>(null)
+  const canvas2 = useRef<HTMLCanvasElement>(null)
+  useMatrixEffect(canvas, canvas2)
+  const router = useRouter()
+
+  return (
+    <div className="absolute inset-0 children:absolute children:inset-0">
+      <canvas ref={canvas} />
+      <canvas ref={canvas2} />
+      <div className="flex flex-col gap-12 items-center justify-center">
+        <img src="/images/logo.svg" className="opacity-50 h-32 object-cover" />
+        <div className="text-2xl">welcome to Ombre wallet</div>
+        <Button
+          btnType="secondary"
+          job={() => {
+            throw new Error('not implemented')
+          }}>
+          proceed
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default WelcomePage
