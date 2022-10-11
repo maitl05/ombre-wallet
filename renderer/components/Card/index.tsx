de6d6d4b4eb51473f63b23eb580a7c9cd69591d4
maitl <mahdiaminigm2@gmail.com>
2022-10-11 23:54:49 +0330

    mod make Card clickable

--- renderer/components/Card/index.tsx
+++ renderer/components/Card/index.tsx
@@ -4,14 +4,22 @@ import type { ClassName } from 'types'
 
 export type CardProps = PropsWithChildren<{
   className?: ClassName
-}>
+}> &
+  Omit<
+    React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLDivElement>,
+      HTMLDivElement
+    >,
+    'className'
+  >
 
 export default function Card({
   className,
   children,
+  ...props
 }: CardProps): React.ReactElement {
   return (
-    <div className={cn('bg-primary-700 rounded-xl p-3', className)}>
+    <div className={cn('bg-primary-700 rounded-xl p-3', className)} {...props}>
       {children}
     </div>
   )
