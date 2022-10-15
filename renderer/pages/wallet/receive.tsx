fcdb10621ae5527982c3853a89dc1ed85482d468
maitl <mahdiaminigm2@gmail.com>
2022-10-15 22:42:17 +0330

    fix receive page

--- renderer/pages/wallet/receive.tsx
+++ renderer/pages/wallet/receive.tsx
@@ -1,56 +1,60 @@
 import WalletLayout from 'components/WalletLayout'
-import Modal from 'components/Modal'
-import { useContext, useState } from 'react'
-import { WalletCtx } from 'contexts/wallet'
-import Card from 'components/Card'
-import AddressPhoto from 'components/AddressPhoto'
+import Address from 'components/Address'
+import { useStore } from 'hooks/observe-store'
+import _ from 'lodash'
+import AddressDetailModal from 'components/AddressDetailModal'
+import { SingleAddress } from 'types/Store'
+import { useState } from 'react'
 
 export default function WalletReceive() {
-  const { wallet } = useContext(WalletCtx)
-  const [open, setOpen] = useState(false)
+  const wallet = useStore('wallet')
+  const [addressDetail, setAddressDetail] = useState<SingleAddress | null>(null)
 
   return (
     <WalletLayout title="Receive">
-      <div className="flex flex-col gap-3 p-2 w-full break-all">
+      <div className="flex flex-col gap-3">
         <span className="text-2xl">My primary address: </span>
-        <Card className={'w-full flex gap-3'}>
-          <AddressPhoto address={wallet?.info.address} dimensions="4rem" />
-          <div className="flex flex-col">
-            <span>{wallet?.info.address}</span>
-            <span>Primary address</span>
-          </div>
-        </Card>
-
-        {wallet?.addressList.used ?? (
-          <div>
-            <span className="text-2xl">My unused addresses: </span>
-            {wallet?.addressList.used.map((add, index) => (
-              <Card key={index} className={'w-full flex gap-3'}>
-                <AddressPhoto address={add} dimensions="4rem" />
-                <div className="flex flex-col">
-                  <span>{add}</span>
-                  <span>Primary address</span>
-                </div>
-              </Card>
-            ))}
-          </div>
-        )}
-
-        <span className="text-2xl">My unused addresses: </span>
-        {wallet?.addressList.unused.map((add, index) => (
-          <Card key={index} className={'w-full flex gap-3'}>
-            <AddressPhoto address={add} dimensions="4rem" />
-            <div className="flex flex-col">
-              <span>{add}</span>
-              <span>Sub address</span>
-            </div>
-          </Card>
-        ))}
-
-        <Modal title="QR code" open={open} onClose={() => setOpen(false)}>
-          hello from modal
-        </Modal>
+        <Address
+          onClick={() => setAddressDetail(wallet.address_list?.primary[0])}
+          address={wallet.address_list?.primary[0]?.address}
+          addressLabel={wallet.address_list?.primary[0].label}
+        />
       </div>
+
+      {!_.isEmpty(wallet.address_list?.used) && (
+        <div className="flex flex-col gap-3">
+          <span className="text-2xl">My used addresses: </span>
+          {wallet.address_list?.used.map((address, index) => (
+            <Address
+              onClick={() => setAddressDetail(address)}
+              key={index}
+              address={address.address}
+              addressLabel={address.label}
+              addressIndex={address.address_index}
+            />
+          ))}
+        </div>
+      )}
+
+      {!_.isEmpty(wallet.address_list?.unused) && (
+        <div className="flex flex-col gap-3">
+          <span className="text-2xl">My unused addresses: </span>
+          {wallet.address_list?.unused.map((address, index) => (
+            <Address
+              onClick={() => setAddressDetail(address)}
+              key={index}
+              address={address.address}
+              addressLabel={address.label}
+              addressIndex={address.address_index}
+            />
+          ))}
+        </div>
+      )}
+
+      <AddressDetailModal
+        addressDetail={addressDetail}
+        closeHandler={() => setAddressDetail(null)}
+      />
     </WalletLayout>
   )
 }
